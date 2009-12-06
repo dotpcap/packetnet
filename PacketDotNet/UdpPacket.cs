8b38dbbfc8773e5b812f21dada35d992d93362b5
Chris Morgan <chmorgan@gmail.com>
2009-12-06 17:56:21 -0500

    Start merging fields from SharpPcap into UdpPacket

--- PacketDotNet/UdpPacket.cs
+++ PacketDotNet/UdpPacket.cs
@@ -18,81 +18,249 @@ along with Packet.Net.  If not, see <http://www.gnu.org/licenses/>.
  *  Copyright 2009 Chris Morgan <chmorgan@gmail.com>
  */
 ﻿using System;
+using Packet.Net.Utils;
 
 namespace Packet.Net
 {
     public class UdpPacket : TransportPacket
     {
-        private ushort packet_length, checksum;
-
-        public override byte[] Bytes
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public ushort SourcePort
+#if false
+        /// <summary> Fetch the port number on the source host.</summary>
+        virtual public int SourcePort
         {
             get
             {
-                if (this.source_port == 0)
-                {
-                    this.SourcePort = BitConverter.ToUInt16(new byte[] { this.Header[1], this.Header[0] }, 0);
-                }
-                return this.source_port;
+                return ArrayHelper.extractInteger(Bytes, _ipPayloadOffset + UDPFields_Fields.UDP_SP_POS, UDPFields_Fields.UDP_PORT_LEN);
             }
+
             set
             {
-                this.source_port = value;
+                ArrayHelper.insertLong(Bytes, value, _ipPayloadOffset + UDPFields_Fields.UDP_SP_POS, UDPFields_Fields.UDP_PORT_LEN);
             }
+
         }
 
-        public ushort DestinationPort
+        /// <summary> Fetch the port number on the target host.</summary>
+        virtual public int DestinationPort
         {
             get
             {
-                if (this.destination_port == 0)
-                {
-                    this.DestinationPort = BitConverter.ToUInt16(new byte[] { this.Header[3], this.Header[2] }, 0);
-                }
-                return this.destination_port;
+                
+                return ArrayHelper.extractInteger(Bytes, _ipPayloadOffset + UDPFields_Fields.UDP_DP_POS, UDPFields_Fields.UDP_PORT_LEN);
             }
+
             set
             {
-                this.destination_port = value;
+                ArrayHelper.insertLong(Bytes, value, _ipPayloadOffset + UDPFields_Fields.UDP_DP_POS, UDPFields_Fields.UDP_PORT_LEN);
             }
         }
-
-        public ushort PacketLength
+        /// <summary> Fetch the total length of the UDP packet, including header and
+        /// data payload, in bytes.
+        /// </summary>
+        virtual public int UDPLength
         {
             get
             {
-                if (this.packet_length == 0)
-                {
-                    this.PacketLength = (ushort)this.PayloadData.Length;
-                }
-                return this.packet_length; 
+                // should produce the same value as header.length + data.length
+                return Length;
             }
+
             set
             {
-                this.packet_length = value; 
+                ArrayHelper.insertLong(Bytes, value, _ipPayloadOffset + UDPFields_Fields.UDP_LEN_POS, UDPFields_Fields.UDP_LEN_LEN);
             }
+
         }
 
-        public ushort Checksum
+        /// <summary> Fetch the header checksum.</summary>
+        virtual public int UDPChecksum
         {
             get
             {
-                if (this.checksum == 0)
+                return GetTransportLayerChecksum(_ipPayloadOffset + UDPFields_Fields.UDP_CSUM_POS);
+            }
+
+            set
+            {
+                SetTransportLayerChecksum(value, UDPFields_Fields.UDP_CSUM_POS);
+            }
+
+        }
+
+        /// <summary> Fetch the UDP header a byte array.</summary>
+        virtual public byte[] UDPHeader
+        {
+            get
+            {
+                if (_udpHeaderBytes == null)
                 {
-                    this.Checksum = BitConverter.ToUInt16(new byte[] { this.Header[7], this.Header[6] }, 0);
+                    _udpHeaderBytes = PacketEncoding.extractHeader(_ipPayloadOffset, UDPFields_Fields.UDP_HEADER_LEN, Bytes);
                 }
-                return this.checksum;
+                return _udpHeaderBytes;
+            }
+
+        }
+
+        /// <summary> Fetch the UDP header length in bytes.</summary>
+        virtual public int UDPHeaderLength
+        {
+            get
+            {
+                return UDPFields_Fields.UDP_HEADER_LEN;
+            }
+        }
+
+        /// <summary> Fetches the length of the payload data.</summary>
+        virtual public int PayloadDataLength
+        {
+            get
+            {
+                return (IPPayloadLength - UDPHeaderLength);
+            }
+        }
+
+        /// <summary> Fetch the UDP data as a byte array.</summary>
+        virtual public byte[] UDPData
+        {
+            get
+            {
+                if (_udpDataBytes == null)
+                {
+                    _udpDataBytes = new byte[PayloadDataLength];
+                    Array.Copy(Bytes, _ipPayloadOffset + UDPHeaderLength, _udpDataBytes, 0, PayloadDataLength);
+                }
+                return _udpDataBytes;
             }
             set
             {
-                this.checksum = value;
+                SetData(value);
+            }
+
+        }
+#endif
+
+        /// <summary> Fetch ascii escape sequence of the color associated with this packet type.</summary>
+        override public System.String Color
+        {
+            get
+            {
+                return AnsiEscapeSequences.LIGHT_GREEN;
             }
         }
+
+#if false
+        /// <summary> Fetch the total length of the UDP packet, including header and
+        /// data payload, in bytes.
+        /// </summary>
+        public int Length
+        {
+            get
+            {
+                // should produce the same value as header.length + data.length
+                return ArrayHelper.extractInteger(Bytes, _ipPayloadOffset + UDPFields_Fields.UDP_LEN_POS, UDPFields_Fields.UDP_LEN_LEN);
+            }
+        }
+
+        /// <summary> Fetch the header checksum.</summary>
+        public int Checksum
+        {
+            get
+            {
+                return UDPChecksum;
+            }
+            set
+            {
+                UDPChecksum=value;
+            }
+        }
+
+        /// <summary> Computes the UDP checksum, optionally updating the UDP checksum header.
+        /// 
+        /// </summary>
+        /// <param name="update">Specifies whether or not to update the UDP checksum header
+        /// after computing the checksum. A value of true indicates the
+        /// header should be updated, a value of false indicates it should
+        /// not be updated.
+        /// </param>
+        /// <returns> The computed UDP checksum.
+        /// </returns>
+        public int ComputeUDPChecksum(bool update)
+        {
+            if (IPVersion != IPVersions.IPv4)
+                throw new System.NotImplementedException("IPVersion of " + IPVersion + " is unrecognized");
+
+            // copy the udp section with data
+            byte[] udp = IPData;
+            // reset the checksum field (checksum is calculated when this field is
+            // zeroed)
+            ArrayHelper.insertLong(udp, 0, UDPFields_Fields.UDP_CSUM_POS, UDPFields_Fields.UDP_CSUM_LEN);
+            //pseudo ip header should be attached to the udp+data
+            udp = AttachPseudoIPHeader(udp);
+            // compute the one's complement sum of the udp header
+            int cs = ChecksumUtils.OnesComplementSum(udp);
+            if (update)
+            {
+                UDPChecksum = cs;
+            }
+
+            return cs;
+        }
+
+        public int ComputeUDPChecksum()
+        {
+            return ComputeUDPChecksum(true);
+        }
+
+        private byte[] _udpHeaderBytes = null;
+
+        private byte[] _udpDataBytes = null;
+#endif
+
+#if false        
+        /// <summary> Fetch the UDP data as a byte array.</summary>
+        public override byte[] Data
+        {
+            get
+            {
+                return UDPData;
+            }
+        }
+#endif
+
+        /// <summary> Convert this UDP packet to a readable string.</summary>
+        public override System.String ToString()
+        {
+            return ToColoredString(false);
+        }
+
+#if false
+        /// <summary> Generate string with contents describing this UDP packet.</summary>
+        /// <param name="colored">whether or not the string should contain ansi
+        /// color escape sequences.
+        /// </param>
+        public override System.String ToColoredString(bool colored)
+        {
+            System.Text.StringBuilder buffer = new System.Text.StringBuilder();
+            buffer.Append('[');
+            if (colored)
+                buffer.Append(Color);
+            buffer.Append("UDPPacket");
+            if (colored)
+                buffer.Append(AnsiEscapeSequences.RESET);
+            buffer.Append(": ");
+            buffer.Append(SourceAddress);
+            buffer.Append('.');
+            buffer.Append(IPPort.getName(SourcePort));
+            buffer.Append(" -> ");
+            buffer.Append(DestinationAddress);
+            buffer.Append('.');
+            buffer.Append(IPPort.getName(DestinationPort));
+            buffer.Append(" l=" + UDPFields_Fields.UDP_HEADER_LEN + "," + (Length - UDPFields_Fields.UDP_HEADER_LEN));
+            buffer.Append(']');
+
+            return buffer.ToString();
+        }
+#endif
 
         new internal static Packet Parse(byte[] bytes)
         {
@@ -103,8 +271,9 @@ namespace Packet.Net
             Array.Copy(bytes, 8, payload, 0, payload.Length);
 
             UdpPacket udp_packet = new UdpPacket();
-            udp_packet.SetPacketHeader(header);
-            udp_packet.PayloadData = payload;
+            throw new System.NotImplementedException();
+//            udp_packet.Header = header;
+//            udp_packet.PayloadData = payload;
 
             // I didn't go any deeper because this a basic parsing
 

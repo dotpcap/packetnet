ad99c39883138652ee24995e457ffe5672920a99
Chris Morgan <chmorgan@gmail.com>
2009-12-09 01:31:07 -0500

    Move bit conversion tests into BitConversionPerformance.cs and add a performance test for EndianBitConverter so we can compare it to the explicit Array.Copy() BitConverter, IPAddress network to host swap test

---
+++ Test/BitConversionPerformance.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Net;
+using System.IO;
+using NUnit.Framework;
+using MiscUtil.IO;
+using MiscUtil.Conversion;
+
+namespace Test
+{
+    [TestFixture]
+    public class BitConversionPerformance
+    {
+        [Test]
+        public void ArrayCopyBitConverterIpAddressPerformance()
+        {
+            byte[] bytes;
+            int testRuns;
+            int startIndex;
+            int expectedValue;
+            ByteSetupMethods.Setup(out bytes, out testRuns, out startIndex,
+                                   out expectedValue);
+
+            var startTime = DateTime.Now;
+
+            for(int i = 0; i < testRuns; i++)
+            {
+                var actualValue = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(bytes, startIndex));
+
+                // NOTE: Assert.AreEqual() significantly slows, by a factor of ~6x
+                //       the execution of this loop, so we perform ourself and
+                //       then call Assert.AreEqual() if the comparison fails.
+                //       This doesn't reduce performance by a noticable amount
+                if(actualValue != expectedValue)
+                {
+                    Assert.AreEqual(expectedValue, actualValue);                    
+                }
+            }
+
+            var endTime = DateTime.Now;
+
+            var rate = new Rate(startTime, endTime, testRuns, "Test runs");
+
+            Console.WriteLine(rate.ToString());
+        }
+
+        [Test]
+        public void EndianReaderWriterPerformance()
+        {
+            byte[] bytes;
+            int testRuns;
+            int startIndex;
+            int expectedValue;
+            ByteSetupMethods.Setup(out bytes, out testRuns, out startIndex,
+                                   out expectedValue);
+
+            var memStream = new MemoryStream(bytes);
+            var endianReader = new EndianBinaryReader(EndianBitConverter.Big, memStream);
+
+            var startTime = DateTime.Now;
+
+            for(int i = 0; i < testRuns; i++)
+            {
+                endianReader.Seek(startIndex, SeekOrigin.Begin);
+                var actualValue = endianReader.ReadInt32();
+
+                // NOTE: Assert.AreEqual() significantly slows, by a factor of ~6x
+                //       the execution of this loop, so we perform ourself and
+                //       then call Assert.AreEqual() if the comparison fails.
+                //       This doesn't reduce performance by a noticable amount
+                if(actualValue != expectedValue)
+                {
+                    Assert.AreEqual(expectedValue, actualValue);
+                }
+            }
+
+            var endTime = DateTime.Now;
+
+            var rate = new Rate(startTime, endTime, testRuns, "Test runs");
+
+            Console.WriteLine(rate.ToString());
+        }
+
+        [Test]
+        public void EndianBitConverterPerformance()
+        {
+            byte[] bytes;
+            int testRuns;
+            int startIndex;
+            int expectedValue;
+            ByteSetupMethods.Setup(out bytes, out testRuns, out startIndex,
+                                   out expectedValue);
+
+            var startTime = DateTime.Now;
+
+            for(int i = 0; i < testRuns; i++)
+            {
+                var actualValue = MiscUtil.Conversion.EndianBitConverter.Big.ToInt32(bytes, startIndex);
+
+                // NOTE: Assert.AreEqual() significantly slows, by a factor of ~6x
+                //       the execution of this loop, so we perform ourself and
+                //       then call Assert.AreEqual() if the comparison fails.
+                //       This doesn't reduce performance by a noticable amount
+                if(actualValue != expectedValue)
+                {
+                    Assert.AreEqual(expectedValue, actualValue);                    
+                }
+            }
+
+            var endTime = DateTime.Now;
+
+            var rate = new Rate(startTime, endTime, testRuns, "Test runs");
+
+            Console.WriteLine(rate.ToString());
+        }
+    }
+}

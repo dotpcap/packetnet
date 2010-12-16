0d0f57a2448d562a7f1813c67c1b061fc4b27fbd
Evan Plaice <Evan.Plaice@gmail.com>
2010-12-15 20:06:59 -0700

    Updated the ARPOperation enum
    ARPOperation.cs
    * added my name to the copyright header
    * added all of the missing fields
    * updated all of the comments

--- PacketDotNet/ARPOperation.cs
+++ PacketDotNet/ARPOperation.cs
@@ -16,6 +16,7 @@ along with PacketDotNet.  If not, see <http://www.gnu.org/licenses/>.
 */
 /*
  *  Copyright 2010 Chris Morgan <chmorgan@gmail.com>
+ *  Copyright 2010 Evan Plaice <evanplaice@gmail.com>
  */
 using System;
 
@@ -24,16 +25,97 @@ namespace PacketDotNet
     /// <summary>
     /// The possible ARP operation values
     /// </summary>
+    /// <remarks>
+    /// References:
+    ///  - http://www.networksorcery.com/enp/default1101.htm
+    /// </remarks>
     public enum ARPOperation : ushort
     {
-        /// <summary>
-        /// Arp request
-        /// </summary>
-        Request = 0x1,
+        /// <summary>Request</summary>
+        /// <remarks>See RFC 826, RFC 5227</remarks>
+        Request = 1,
 
-        /// <summary>
-        /// Arp response
-        /// </summary>
-        Response = 0x2
+        /// <summary>Response</summary>
+        /// <remarks>See RFC 826, RFC 1868, RFC 5227</remarks>
+        Response = 2,
+
+        /// <summary>Request Reverse</summary>
+        /// <remarks>See RFC 903</remarks>
+        RequestReverse = 3,
+
+        /// <summary>Reply Reverse</summary>
+        /// <remarks>See RFC 903</remarks>
+        ReplyReverse = 4,
+
+        /// <summary>DRARP Request</summary>
+        /// <remarks>See RFC 1931</remarks>
+        DRARPRequest = 5,
+
+        /// <summary>DRARP Reply</summary>
+        /// <remarks>See RFC 1931</remarks>
+        DRARPReply = 6,
+
+        /// <summary>DRARP Error</summary>
+        /// <remarks>See RFC 1931</remarks>
+        DRARPError = 7,
+
+        /// <summary>InARP Request</summary>
+        /// <remarks>See RFC 1293</remarks>
+        InARPRequest = 8,
+
+        /// <summary>InARP Reply</summary>
+        /// <remarks>See RFC 1293</remarks>
+        InARPReply = 9,
+
+        /// <summary>ARP NAK</summary>
+        /// <remarks>See RFC 1577</remarks>
+        ARPNAK = 10,
+
+        /// <summary>MARS Request</summary>
+        MARSRequest = 11,
+
+        /// <summary>MARS Multi</summary>
+        MARSMulti = 12,
+
+        /// <summary>MARS MServ</summary>
+        MARSMServ = 13,
+
+        /// <summary>MARS Join</summary>
+        MARSJoin = 14,
+
+        /// <summary>MARS Leave</summary>
+        MARSLeave = 15,
+
+        /// <summary>MARS NAK</summary>
+        MARSNAK = 16,
+
+        /// <summary>MARS Unserv</summary>
+        MARSUnserv = 17,
+
+        /// <summary>MARS SJoin</summary>
+        MARSSJoin = 18,
+
+        /// <summary>MARS SLeave</summary>
+        MARSSLeave = 19,
+
+        /// <summary>MARS Grouplist Request</summary>
+        MARSGrouplistRequest = 20,
+
+        /// <summary>MARS Grouplist Reply</summary>
+        MARSGrouplistReply = 21,
+
+        /// <summary>MARS Redirect Map</summary>
+        MARSRedirectMap = 22,
+
+        /// <summary>MARS UNARP</summary>
+        /// <remarks>See RFC 2176</remarks>
+        MaposUnarp = 23,
+
+        /// <summary>OP_EXP1</summary>
+        /// <remarks>See RFC 5494</remarks>
+        OP_EXP1 = 24,
+
+        /// <summary>OP_EXP2</summary>
+        OP_EXP2 = 25
     }
 }

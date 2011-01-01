462d6642e777c37357a7dba96a3d66680fb54a2d
Chris Morgan <chmorgan@gmail.com>
2011-01-01 11:03:27 -0500

    Add test captures for the three different packet formats captured by AirPcap and basic tests just to see what happens if those packets are parsed

---
+++ Test/PacketType/Ieee80211RadioPacketTest.cs
@@ -0,0 +1,52 @@
+﻿/*
+This file is part of PacketDotNet
+
+PacketDotNet is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+PacketDotNet is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with PacketDotNet.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
+ *  Copyright 2010 Chris Morgan <chmorgan@gmail.com>
+ */
+
+using System;
+using NUnit.Framework;
+using SharpPcap;
+using PacketDotNet;
+
+namespace Test.PacketType
+{
+    [TestFixture]
+    public class Ieee80211RadioPacketTest
+    {
+        /// <summary>
+        /// Test that parsing an ip packet yields the proper field values
+        /// </summary>
+        [Test]
+        public void ReadingPacketsFromFile()
+        {
+            var dev = new OfflinePcapDevice("../../CaptureFiles/80211_plus_radiotap_header.pcap");
+            dev.Open();
+            var rawPacket = dev.GetNextRawPacket();
+            dev.Close();
+
+            Packet p = Packet.ParsePacket((LinkLayers)rawPacket.LinkLayerType,
+                                          new PosixTimeval(rawPacket.Timeval.Seconds,
+                                                           rawPacket.Timeval.MicroSeconds),
+                                          rawPacket.Data);
+
+            Assert.IsNotNull(p);
+
+            Console.WriteLine(p.ToString());
+        }
+    }
+}

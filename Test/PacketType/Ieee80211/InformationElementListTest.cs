1955482fe984910e2cce7ac38c9f1aaff616c970
Alan Rushforth <alan.rushforth@gmail.com>
2012-01-08 20:48:49 +0000

    Adding unit tests for new accessor convenience methods

--- Test/PacketType/Ieee80211/InformationElementListTest.cs
+++ Test/PacketType/Ieee80211/InformationElementListTest.cs
@@ -15,34 +15,36 @@ namespace Test.PacketType
         [TestFixture]
         public class InformationElementListTest
         {
-//            [Test]
-//            public void Test_Constuctor_FromBeaconFrame ()
-//            {
-//                var dev = new CaptureFileReaderDevice ("../../CaptureFiles/80211_beacon_frame.pcap");
-//                dev.Open ();
-//                var rawCapture = dev.GetNextPacket ();
-//                dev.Close ();
-//
-//                Packet p = Packet.ParsePacket (rawCapture.LinkLayerType, rawCapture.Data);
-//                BeaconFrame beaconFrame = (BeaconFrame)p.PayloadPacket;
-//                
-//                List<InformationElement> infoElements = beaconFrame.InformationElements.InformationElements;
-//                Assert.AreEqual(InformationElement.ElementId.ServiceSetIdentity, infoElements[0].Id);
-//                Assert.AreEqual(InformationElement.ElementId.SupportedRates, infoElements[1].Id);
-//                Assert.AreEqual(InformationElement.ElementId.DsParameterSet, infoElements[2].Id);
-//                Assert.AreEqual(InformationElement.ElementId.TrafficIndicationMap, infoElements[3].Id);
-//                Assert.AreEqual(InformationElement.ElementId.ErpInformation, infoElements[4].Id);
-//                Assert.AreEqual(InformationElement.ElementId.ErpInformation2, infoElements[5].Id);
-//                Assert.AreEqual(InformationElement.ElementId.RobustSecurityNetwork, infoElements[6].Id);
-//                Assert.AreEqual(InformationElement.ElementId.ExtendedSupportedRates, infoElements[7].Id);
-//                Assert.AreEqual(InformationElement.ElementId.HighThroughputCapabilities, infoElements[8].Id);
-//                Assert.AreEqual(InformationElement.ElementId.HighThroughputInformation, infoElements[9].Id);
-//                Assert.AreEqual(InformationElement.ElementId.VendorSpecific, infoElements[10].Id);
-//                Assert.AreEqual(InformationElement.ElementId.VendorSpecific, infoElements[11].Id);
-//                Assert.AreEqual(InformationElement.ElementId.VendorSpecific, infoElements[12].Id);
-//                Assert.AreEqual(InformationElement.ElementId.VendorSpecific, infoElements[13].Id);
-//                Assert.AreEqual(InformationElement.ElementId.VendorSpecific, infoElements[14].Id);
-//            }
+            
+            [Test]
+            public void Test_Constuctor_FromBeaconFrame ()
+            {
+                var dev = new CaptureFileReaderDevice ("../../CaptureFiles/80211_beacon_frame.pcap");
+                dev.Open ();
+                var rawCapture = dev.GetNextPacket ();
+                dev.Close ();
+
+                Packet p = Packet.ParsePacket (rawCapture.LinkLayerType, rawCapture.Data);
+                BeaconFrame beaconFrame = (BeaconFrame)p.PayloadPacket;
+                
+                List<InformationElement> infoElements = beaconFrame.InformationElements;
+                Assert.AreEqual (InformationElement.ElementId.ServiceSetIdentity, infoElements [0].Id);
+                Assert.AreEqual (InformationElement.ElementId.SupportedRates, infoElements [1].Id);
+                Assert.AreEqual (InformationElement.ElementId.DsParameterSet, infoElements [2].Id);
+                Assert.AreEqual (InformationElement.ElementId.TrafficIndicationMap, infoElements [3].Id);
+                Assert.AreEqual (InformationElement.ElementId.ErpInformation, infoElements [4].Id);
+                Assert.AreEqual (InformationElement.ElementId.ErpInformation2, infoElements [5].Id);
+                Assert.AreEqual (InformationElement.ElementId.RobustSecurityNetwork, infoElements [6].Id);
+                Assert.AreEqual (InformationElement.ElementId.ExtendedSupportedRates, infoElements [7].Id);
+                Assert.AreEqual (InformationElement.ElementId.HighThroughputCapabilities, infoElements [8].Id);
+                Assert.AreEqual (InformationElement.ElementId.HighThroughputInformation, infoElements [9].Id);
+                Assert.AreEqual (InformationElement.ElementId.VendorSpecific, infoElements [10].Id);
+                Assert.AreEqual (InformationElement.ElementId.VendorSpecific, infoElements [11].Id);
+                Assert.AreEqual (InformationElement.ElementId.VendorSpecific, infoElements [12].Id);
+                Assert.AreEqual (InformationElement.ElementId.VendorSpecific, infoElements [13].Id);
+                Assert.AreEqual (InformationElement.ElementId.VendorSpecific, infoElements [14].Id);
+            }
+            
             
             [Test]
             public void Test_Constructor_EmptyByteArray ()
@@ -68,13 +70,13 @@ namespace Test.PacketType
                 Assert.AreEqual (2, ieList.Count);
                 Assert.AreEqual (13, ieList.Length);
 
-                Assert.AreEqual (InformationElement.ElementId.WifiProtectedAccess, ieList[0].Id);
-                Assert.AreEqual (5, ieList[0].ValueLength);
-                Assert.IsTrue (ieList[0].Value.SequenceEqual (new Byte[] { 0x1, 0x2, 0x3, 0x4, 0x5 }));
+                Assert.AreEqual (InformationElement.ElementId.WifiProtectedAccess, ieList [0].Id);
+                Assert.AreEqual (5, ieList [0].ValueLength);
+                Assert.IsTrue (ieList [0].Value.SequenceEqual (new Byte[] { 0x1, 0x2, 0x3, 0x4, 0x5 }));
 
                 Assert.AreEqual (InformationElement.ElementId.VendorSpecific, ieList [1].Id);
-                Assert.AreEqual (4, ieList[1].ValueLength);
-                Assert.IsTrue (ieList[1].Value.SequenceEqual (new Byte[] { 0xFF, 0xFE, 0xFD, 0xFC }));
+                Assert.AreEqual (4, ieList [1].ValueLength);
+                Assert.IsTrue (ieList [1].Value.SequenceEqual (new Byte[] { 0xFF, 0xFE, 0xFD, 0xFC }));
             }
 
             [Test]
@@ -105,8 +107,8 @@ namespace Test.PacketType
                     InformationElement.ElementId.VendorSpecific, new Byte[] { 0xFF, 0xFE, 0xFD, 0xFC });
           
                 InformationElementList ieList = new InformationElementList ();
-                ieList.Add(ie1);
-                ieList.Add(ie2);
+                ieList.Add (ie1);
+                ieList.Add (ie2);
 
                 Byte[] expectedBytes = new Byte[] { 0xD3, 0x5, 0x1, 0x2, 0x3, 0x4, 0x5, 0xDD, 0x4, 0xFF, 0xFE, 0xFD, 0xFC };
 
@@ -127,6 +129,61 @@ namespace Test.PacketType
                 Assert.AreEqual (0xFC, b [12]);
 
                 Assert.IsTrue (ieList.Bytes.SequenceEqual (expectedBytes));
+            }
+            
+            [Test]
+            public void Test_FindFirstById_FindElement ()
+            {
+                BeaconFrame beaconFrame = LoadBeaconFrameFromFile("../../CaptureFiles/80211_beacon_frame.pcap");
+                            
+                InformationElement infoElement = beaconFrame.InformationElements.FindFirstById (InformationElement.ElementId.DsParameterSet);
+                Assert.AreEqual (InformationElement.ElementId.DsParameterSet, infoElement.Id);
+            }
+            
+            [Test]
+            public void Test_FindFirstById_ElementNotPresent ()
+            {
+                BeaconFrame beaconFrame = LoadBeaconFrameFromFile ("../../CaptureFiles/80211_beacon_frame.pcap");
+                            
+                InformationElement infoElement = beaconFrame.InformationElements.FindFirstById (InformationElement.ElementId.ChallengeText);
+                Assert.IsNull (infoElement);
+            }
+            
+            [Test]
+            public void Test_FindById_FindRepeatedElement ()
+            {
+                BeaconFrame beaconFrame = LoadBeaconFrameFromFile ("../../CaptureFiles/80211_beacon_frame.pcap");
+                            
+                InformationElement[] infoElements = beaconFrame.InformationElements.FindById (InformationElement.ElementId.VendorSpecific);
+                
+                Assert.AreEqual (5, infoElements.Length);              
+                
+                Assert.AreEqual (InformationElement.ElementId.VendorSpecific, infoElements [0].Id);
+                Assert.AreEqual (InformationElement.ElementId.VendorSpecific, infoElements [1].Id);
+                Assert.AreEqual (InformationElement.ElementId.VendorSpecific, infoElements [2].Id);
+                Assert.AreEqual (InformationElement.ElementId.VendorSpecific, infoElements [3].Id);
+                Assert.AreEqual (InformationElement.ElementId.VendorSpecific, infoElements [4].Id);
+            }
+            
+            [Test]
+            public void Test_FindById_ElementNotPresent ()
+            {
+                BeaconFrame beaconFrame = LoadBeaconFrameFromFile ("../../CaptureFiles/80211_beacon_frame.pcap");
+                            
+                InformationElement[] infoElements = beaconFrame.InformationElements.FindById (InformationElement.ElementId.ChallengeText);
+                Assert.AreEqual (0, infoElements.Length);
+            }
+            
+            private BeaconFrame LoadBeaconFrameFromFile (String file)
+            {
+                var dev = new CaptureFileReaderDevice (file);
+                dev.Open ();
+                var rawCapture = dev.GetNextPacket ();
+                dev.Close ();
+
+                Packet p = Packet.ParsePacket (rawCapture.LinkLayerType, rawCapture.Data);
+                BeaconFrame beaconFrame = (BeaconFrame)p.PayloadPacket;
+                return beaconFrame;
             }
         }
     }

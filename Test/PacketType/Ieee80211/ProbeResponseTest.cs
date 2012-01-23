8f42c316728f484855821d577b80b8b80b77fe4b
Alan Rushforth <alan.rushforth@gmail.com>
2012-01-23 18:52:38 +0000

    Adding ProbeResponseFrame constructor for creating packets programmatically

--- Test/PacketType/Ieee80211/ProbeResponseTest.cs
+++ Test/PacketType/Ieee80211/ProbeResponseTest.cs
@@ -24,6 +24,7 @@ using SharpPcap.LibPcap;
 using PacketDotNet;
 using PacketDotNet.Utils;
 using PacketDotNet.Ieee80211;
+using System.Net.NetworkInformation;
 
 namespace Test.PacketType
 {
@@ -36,49 +37,105 @@ namespace Test.PacketType
             /// Test that parsing a probe response frame yields the proper field values
             /// </summary>
             [Test]
-            public void Test_Constructor()
+            public void Test_Constructor ()
             {
-                var dev = new CaptureFileReaderDevice("../../CaptureFiles/80211_probe_response_frame.pcap");
-                dev.Open();
-                var rawCapture = dev.GetNextPacket();
-                dev.Close();
+                var dev = new CaptureFileReaderDevice ("../../CaptureFiles/80211_probe_response_frame.pcap");
+                dev.Open ();
+                var rawCapture = dev.GetNextPacket ();
+                dev.Close ();
 
-                Packet p = Packet.ParsePacket(rawCapture.LinkLayerType, rawCapture.Data);
+                Packet p = Packet.ParsePacket (rawCapture.LinkLayerType, rawCapture.Data);
                 ProbeResponseFrame frame = (ProbeResponseFrame)p.PayloadPacket;
 
-                Assert.AreEqual(0, frame.FrameControl.ProtocolVersion);
-                Assert.AreEqual(FrameControlField.FrameTypes.ManagementProbeResponse, frame.FrameControl.Type);
-                Assert.IsFalse(frame.FrameControl.ToDS);
-                Assert.IsFalse(frame.FrameControl.FromDS);
-                Assert.IsFalse(frame.FrameControl.MoreFragments);
-                Assert.IsTrue(frame.FrameControl.Retry);
-                Assert.IsFalse(frame.FrameControl.PowerManagement);
-                Assert.IsFalse(frame.FrameControl.MoreData);
-                Assert.IsFalse(frame.FrameControl.Wep);
-                Assert.IsFalse(frame.FrameControl.Order);
-                Assert.AreEqual(314, frame.Duration.Field); //this need expanding on in the future
-                Assert.AreEqual("0020008AB749", frame.DestinationAddress.ToString().ToUpper());
-                Assert.AreEqual("00223FCD9C26", frame.SourceAddress.ToString().ToUpper());
-                Assert.AreEqual("00223FCD9C26", frame.BssId.ToString().ToUpper());
-                Assert.AreEqual(0, frame.SequenceControl.FragmentNumber);
-                Assert.AreEqual(1468, frame.SequenceControl.SequenceNumber);
+                Assert.AreEqual (0, frame.FrameControl.ProtocolVersion);
+                Assert.AreEqual (FrameControlField.FrameTypes.ManagementProbeResponse, frame.FrameControl.Type);
+                Assert.IsFalse (frame.FrameControl.ToDS);
+                Assert.IsFalse (frame.FrameControl.FromDS);
+                Assert.IsFalse (frame.FrameControl.MoreFragments);
+                Assert.IsTrue (frame.FrameControl.Retry);
+                Assert.IsFalse (frame.FrameControl.PowerManagement);
+                Assert.IsFalse (frame.FrameControl.MoreData);
+                Assert.IsFalse (frame.FrameControl.Wep);
+                Assert.IsFalse (frame.FrameControl.Order);
+                Assert.AreEqual (314, frame.Duration.Field); //this need expanding on in the future
+                Assert.AreEqual ("0020008AB749", frame.DestinationAddress.ToString ().ToUpper ());
+                Assert.AreEqual ("00223FCD9C26", frame.SourceAddress.ToString ().ToUpper ());
+                Assert.AreEqual ("00223FCD9C26", frame.BssId.ToString ().ToUpper ());
+                Assert.AreEqual (0, frame.SequenceControl.FragmentNumber);
+                Assert.AreEqual (1468, frame.SequenceControl.SequenceNumber);
 
-                Assert.AreEqual(0x0000047A44EF1DE0, frame.Timestamp);
-                Assert.AreEqual(100, frame.BeaconInterval);
+                Assert.AreEqual (0x0000047A44EF1DE0, frame.Timestamp);
+                Assert.AreEqual (100, frame.BeaconInterval);
 
-                Assert.IsTrue(frame.CapabilityInformation.IsEss);
-                Assert.IsFalse(frame.CapabilityInformation.IsIbss);
-                Assert.IsFalse(frame.CapabilityInformation.CfPollable);
-                Assert.IsFalse(frame.CapabilityInformation.CfPollRequest);
-                Assert.IsTrue(frame.CapabilityInformation.Privacy);
-                Assert.IsFalse(frame.CapabilityInformation.ShortPreamble);
-                Assert.IsFalse(frame.CapabilityInformation.Pbcc);
-                Assert.IsFalse(frame.CapabilityInformation.ChannelAgility);
-                Assert.IsTrue(frame.CapabilityInformation.ShortTimeSlot);
-                Assert.IsFalse(frame.CapabilityInformation.DssOfdm);
+                Assert.IsTrue (frame.CapabilityInformation.IsEss);
+                Assert.IsFalse (frame.CapabilityInformation.IsIbss);
+                Assert.IsFalse (frame.CapabilityInformation.CfPollable);
+                Assert.IsFalse (frame.CapabilityInformation.CfPollRequest);
+                Assert.IsTrue (frame.CapabilityInformation.Privacy);
+                Assert.IsFalse (frame.CapabilityInformation.ShortPreamble);
+                Assert.IsFalse (frame.CapabilityInformation.Pbcc);
+                Assert.IsFalse (frame.CapabilityInformation.ChannelAgility);
+                Assert.IsTrue (frame.CapabilityInformation.ShortTimeSlot);
+                Assert.IsFalse (frame.CapabilityInformation.DssOfdm);
 
-                Assert.AreEqual(0x257202BE, frame.FrameCheckSequence);
-                Assert.AreEqual(164, frame.FrameSize);
+                Assert.AreEqual (0x257202BE, frame.FrameCheckSequence);
+                Assert.AreEqual (164, frame.FrameSize);
+            }
+            
+            [Test]
+            public void Test_Constructor_ConstructWithValues ()
+            {
+                InformationElement ssidInfoElement = new InformationElement (InformationElement.ElementId.ServiceSetIdentity, 
+                                                                           new Byte[] { 0x68, 0x65, 0x6c, 0x6c, 0x6f });
+                InformationElement vendorElement = new InformationElement (InformationElement.ElementId.VendorSpecific,
+                                                                           new Byte[] {0x01, 0x02, 0x03, 0x04, 0x05});
+                
+                
+                ProbeResponseFrame frame = new ProbeResponseFrame (PhysicalAddress.Parse ("111111111111"),
+                                                                   PhysicalAddress.Parse ("222222222222"),
+                                                                   PhysicalAddress.Parse ("333333333333"),
+                                                                   new InformationElementList (){ssidInfoElement, vendorElement});
+                
+                frame.FrameControl.ToDS = false;
+                frame.FrameControl.FromDS = true;
+                frame.FrameControl.MoreFragments = true;
+                
+                frame.Duration.Field = 0x1234;
+                
+                frame.SequenceControl.SequenceNumber = 0x77;
+                frame.SequenceControl.FragmentNumber = 0x1;
+                
+                frame.CapabilityInformation.IsEss = true;
+                frame.CapabilityInformation.ChannelAgility = true;
+                
+                frame.FrameCheckSequence = 0x01020304;
+                
+                //serialize the frame into a byte buffer
+                var bytes = frame.Bytes;
+                var bas = new ByteArraySegment (bytes);
+    
+                //create a new frame that should be identical to the original
+                ProbeResponseFrame recreatedFrame = new ProbeResponseFrame (bas);
+                
+                Assert.AreEqual (FrameControlField.FrameTypes.ManagementProbeResponse, recreatedFrame.FrameControl.Type);
+                Assert.IsFalse (recreatedFrame.FrameControl.ToDS);
+                Assert.IsTrue (recreatedFrame.FrameControl.FromDS);
+                Assert.IsTrue (recreatedFrame.FrameControl.MoreFragments);
+                
+                Assert.AreEqual (0x77, recreatedFrame.SequenceControl.SequenceNumber);
+                Assert.AreEqual (0x1, recreatedFrame.SequenceControl.FragmentNumber);
+                
+                Assert.IsTrue (frame.CapabilityInformation.IsEss);
+                Assert.IsTrue (frame.CapabilityInformation.ChannelAgility);
+                
+                Assert.AreEqual ("111111111111", recreatedFrame.SourceAddress.ToString ().ToUpper ());
+                Assert.AreEqual ("222222222222", recreatedFrame.DestinationAddress.ToString ().ToUpper ());
+                Assert.AreEqual ("333333333333", recreatedFrame.BssId.ToString ().ToUpper ());
+                
+                Assert.AreEqual (ssidInfoElement, recreatedFrame.InformationElements [0]);
+                Assert.AreEqual (vendorElement, recreatedFrame.InformationElements [1]);
+                
+                Assert.AreEqual (0x01020304, recreatedFrame.FrameCheckSequence);
             }
         } 
     }

149528868faebab9dcc34e236c2dead503d91ace
Alan Rushforth <alan.rushforth@gmail.com>
2012-02-07 23:08:42 +0000

    Adding copyright notices to files I have added to the project.

--- PacketDotNet/Ieee80211/DurationField.cs
+++ PacketDotNet/Ieee80211/DurationField.cs
@@ -14,6 +14,9 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with PacketDotNet.  If not, see <http://www.gnu.org/licenses/>.
 */
+/*
+ * Copyright 2012 Alan Rushforth <alan.rushforth@gmail.com>
+ */
 
 using System;
 using System.Collections.Generic;

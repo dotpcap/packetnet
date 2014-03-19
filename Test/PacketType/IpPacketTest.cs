8c7f8e45c0390a827b86588784cdd32694afae8f
Rune Jacobsen <havremunken@gmail.com>
2014-03-19 12:40:07 +0100

    Add capture file and unit tests to verify correct function of IP fragmentation handling

--- Test/PacketType/IpPacketTest.cs
+++ Test/PacketType/IpPacketTest.cs
@@ -49,5 +49,50 @@ namespace Test.PacketType
             Assert.AreEqual(20, ip.Header.Length, "Header.Length doesn't match expected length");
             Console.WriteLine(ip.ToString());
         }
+
+        [Test]
+        public void IpPacket_WhenGettingPacketFragment_LeavesPacketAsPayload()
+        {
+            // Arrange
+            var dev = new CaptureFileReaderDevice("../../CaptureFiles/udp-ip-fragmented.pcap");
+            dev.Open();
+            var firstRawPacket = dev.GetNextPacket();
+            var secondRawPacket = dev.GetNextPacket();
+            dev.Close();
+
+            // Act
+            Packet.ParsePacket(firstRawPacket.LinkLayerType, firstRawPacket.Data); // read and discard first packet
+            var secondPacket = Packet.ParsePacket(secondRawPacket.LinkLayerType, secondRawPacket.Data);
+            var ipFragmentPacket = secondPacket.Extract(typeof(IpPacket)) as IpPacket;
+
+            // Assert
+            Assert.IsNotNull(ipFragmentPacket, "The second packet should contain an IP packet within");
+            Assert.IsNull(ipFragmentPacket.PayloadPacket, "Since packet is IP fragment, we should not have extracted a child packet");
+            Assert.AreEqual(497, ipFragmentPacket.PayloadLength, "The correct payload length for this particular packet should be 497");
+            Assert.AreEqual(497, ipFragmentPacket.PayloadData.Length, "The correct payload length for this particular packet should be 497");
+        }
+
+        [Test]
+        public void IpPacket_WhenGettingPacketThatIsNotAFragment_ParsesPacketProperly()
+        {
+            // Arrange
+            var dev = new CaptureFileReaderDevice("../../CaptureFiles/udp-ip-fragmented.pcap");
+            dev.Open();
+            var firstRawPacket = dev.GetNextPacket();
+            dev.Close();
+
+            // Act
+            var firstPacket = Packet.ParsePacket(firstRawPacket.LinkLayerType, firstRawPacket.Data);
+            var ipPacket = firstPacket.Extract(typeof(IpPacket)) as IpPacket;
+            var udpPacket = ipPacket.Extract(typeof (UdpPacket)) as UdpPacket;
+
+            // Assert
+            Assert.IsNotNull(ipPacket, "The packet should contain an IP packet within");
+            Assert.IsNotNull(ipPacket.PayloadPacket, "This is not a fragment, and should contain an UDP packet");
+            Assert.IsNotNull(udpPacket, "We should have a UDP packet in there");
+            Assert.AreEqual(1977, udpPacket.Length, "We should have the proper length for the packet");
+            Assert.AreEqual(5060, udpPacket.DestinationPort, "We should have extracted the correct destination port");
+            Assert.AreEqual(5060, udpPacket.SourcePort, "We should have extracted the correct source port");
+        }
     }
 }

0c33b469e0f2c068809167f315b5c4cd5f3d91ee
Cameron Elliott <cameron@limevideo.com>
2016-11-29 14:59:14 -0800

    Changes to support Raw IP packets as written by Wireshark and Rawcap.exe(http://www.netresec.com/?page=RawCap)
    Raw IP packets are IPv4 and IPv6 directly inside the raw libpcap packet, the first
    byte of the Raw IP packet is the first byte of the IPv4 or IPv6 packet.
    
    Search for 'LINKTYPE_RAW' on: http://www.tcpdump.org/linktypes.html
    
    This is the file format you get when you use Rawcap.exe to capture packets on the loopback interface.
    More: http://www.netresec.com/?page=RawCap
    
    Winpcap cannot normally capture off of the loopback interface, so this is a nice tool to capture loopback packets, and with this support
    for Raw linklayer these can now be parsed.
    
    It is quite possible this is the link layer used by Wireshark and other tools on Linux when capturing
    from the loopback interface, although they can use LinuxSLL as an option.

---
+++ PacketDotNet/RawIPPacketProtocol.cs
@@ -0,0 +1,38 @@
+﻿/*
+This file is part of PacketDotNet
+
+PacketDotNet is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+PacketDotNet is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with PacketDotNet.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
+ *  Copyright 2010 Chris Morgan <chmorgan@gmail.com>
+ *  Copyright 2010 Cameron Elliott <cameron/at/cameronelliott/dot/com>
+ *  
+ */
+using System;
+namespace PacketDotNet
+{
+    /// <summary>
+    /// Indicates the protocol encapsulated by the PPP packet
+    /// See http://www.iana.org/assignments/ppp-numbers
+    /// </summary>
+    public enum RawIPPacketProtocol : ushort
+    {
+        /// <summary> IPv4 </summary>
+        IPv4 = 4,
+
+        /// <summary> IPv6 </summary>
+        IPv6 = 6,
+    }
+}
+

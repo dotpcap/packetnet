eb4ebe296eb707e2a7603ed1a6fc8b762bf9e60b
Chris Morgan <chmorgan@gmail.com>
2017-09-21 13:35:07 -0400

    RadioTapFields - Remove debug console write that was left in RxFlagsRadioTapField()

--- PacketDotNet/Ieee80211/RadioTapFields.cs
+++ PacketDotNet/Ieee80211/RadioTapFields.cs
@@ -1007,7 +1007,6 @@ namespace PacketDotNet
             public RxFlagsRadioTapField(BinaryReader br)
             {
                 UInt16 flags = br.ReadUInt16();
-                Console.WriteLine("RxFlagsRadioTapField {0}", flags);
                 PlcpCrcCheckFailed = ((flags & 0x2) == 0x2);
             }
             

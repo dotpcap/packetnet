1554c432374daf2111cc4f452025f04898351685
Phyxion <phyxion@users.noreply.github.com>
2018-07-12 17:27:45 +0200

    Improve checksum calculation performance
    Depending on size of the packet, this is about 2-3x as fast as the current code.

--- PacketDotNet/Utils/ChecksumUtils.cs
+++ PacketDotNet/Utils/ChecksumUtils.cs
@@ -39,12 +39,11 @@ namespace PacketDotNet.Utils
         /// Computes the one's complement sum on a byte array
         /// </summary>
         /// <param name="bytes">The bytes.</param>
-        /// <param name="padOddBytesToLeft">if set to <c>true</c> pads the last byte to the left if it has an odd index.</param>
         /// <returns><see cref="Int32" />.</returns>
-        public static Int32 OnesComplementSum(Byte[] bytes, bool padOddBytesToLeft = true)
+        public static Int32 OnesComplementSum(Byte[] bytes)
         {
-            //just complement the one's sum
-            return OnesComplementSum(bytes, 0, bytes.Length, padOddBytesToLeft);
+            // Just complement the one's sum.
+            return OnesComplementSum(bytes, 0, bytes.Length);
         }
 
         /// <summary>
@@ -53,12 +52,11 @@ namespace PacketDotNet.Utils
         /// <param name="bytes">The bytes.</param>
         /// <param name="start">The start.</param>
         /// <param name="len">The length.</param>
-        /// <param name="padOddBytesToLeft">if set to <c>true</c> pads the last byte to the left if it has an odd index.</param>
         /// <returns><see cref="Int32" />.</returns>
-        public static Int32 OnesComplementSum(Byte[] bytes, Int32 start, Int32 len, bool padOddBytesToLeft = true)
+        public static Int32 OnesComplementSum(Byte[] bytes, Int32 start, Int32 len)
         {
-            //just complement the one's sum
-            return ~OnesSum(bytes, start, len, padOddBytesToLeft) & 0xFFFF;
+            // Just complement the one's sum.
+            return ~OnesSum(bytes, start, len);
         }
 
         /// <summary>
@@ -66,21 +64,19 @@ namespace PacketDotNet.Utils
         /// </summary>
         /// <param name="byteArraySegment">The byte array segment.</param>
         /// <param name="prefixedBytes">The prefixed bytes.</param>
-        /// <param name="padOddBytesToLeft">if set to <c>true</c> pads the last byte to the left if it has an odd index.</param>
         /// <returns><see cref="Int32" />.</returns>
-        public static Int32 OnesComplementSum(ByteArraySegment byteArraySegment, byte[] prefixedBytes, bool padOddBytesToLeft = true)
+        public static Int32 OnesComplementSum(ByteArraySegment byteArraySegment, byte[] prefixedBytes)
         {
-            //just complement the one's sum
-            return ~OnesSum(byteArraySegment, prefixedBytes, padOddBytesToLeft) & 0xFFFF;
+            // Just complement the one's sum.
+            return ~OnesSum(byteArraySegment, prefixedBytes);
         }
 
         /// <summary>
         /// Compute a ones sum of a byte array
         /// </summary>
         /// <param name="bytes">A <see cref="System.Byte" /></param>
-        /// <param name="padOddBytesToLeft">if set to <c>true</c> pads the last byte to the left if it has an odd index.</param>
         /// <returns>A <see cref="System.Int32" /></returns>
-        public static Int32 OnesSum(Byte[] bytes, bool padOddBytesToLeft = true)
+        public static Int32 OnesSum(Byte[] bytes)
         {
             return OnesSum(bytes, 0, bytes.Length);
         }
@@ -92,32 +88,20 @@ namespace PacketDotNet.Utils
         /// <param name="bytes">A <see cref="System.Byte" /></param>
         /// <param name="start">A <see cref="System.Int32" /></param>
         /// <param name="len">A <see cref="System.Int32" /></param>
-        /// <param name="padOddBytesToLeft">if set to <c>true</c> pads the last byte to the left if it has an odd index.</param>
         /// <returns>A <see cref="System.Int32" /></returns>
-        public static Int32 OnesSum(Byte[] bytes, Int32 start, Int32 len, bool padOddBytesToLeft = true)
+        public static Int32 OnesSum(Byte[] bytes, Int32 start, Int32 len)
         {
-            var sum = 0;
-            for (var i = start; i < start + len - 1; i += 2)
+            unsafe
             {
-                sum += bytes[i] << 8 | bytes[i + 1];
-            }
+                fixed (byte* mainFixed = bytes)
+                {
+                    var mainArray = mainFixed;
+                    if (start > 0)
+                        mainArray += start;
 
-            // if we have a remaining byte we should add it
-            if (len % 2 == 1)
-            {
-                if (padOddBytesToLeft)
-                    sum += bytes[start + len - 1] << 8;
-                else
-                    sum += bytes[start + len - 1];
+                    return OnesSum(mainArray, len, (byte*)IntPtr.Zero, 0);
+                }
             }
-
-            // fold the sum into 16 bits
-            while (sum >> 16 != 0)
-            {
-                sum = (sum & 0xffff) + (sum >> 16);
-            }
-
-            return sum;
         }
 
         /// <summary>
@@ -126,51 +110,131 @@ namespace PacketDotNet.Utils
         /// </summary>
         /// <param name="byteArraySegment">A <see cref="ByteArraySegment" />.</param>
         /// <param name="prefixedBytes">The prefixed bytes.</param>
-        /// <param name="padOddBytesToLeft">if set to <c>true</c> pads the last byte to the left if it has an odd index.</param>
         /// <returns>A <see cref="System.Int32" /></returns>
-        public static int OnesSum(ByteArraySegment byteArraySegment, byte[] prefixedBytes, bool padOddBytesToLeft = true)
+        public static UInt16 OnesSum(ByteArraySegment byteArraySegment, byte[] prefixedBytes)
         {
-            var sum = 0;
-            for (var i = 0; i < prefixedBytes.Length; i += 2)
+            unsafe
             {
-                sum += prefixedBytes[i] << 8 | prefixedBytes[i + 1];
+                fixed (byte* mainFixed = byteArraySegment.Bytes)
+                {
+                    fixed (byte* prefixedFixed = prefixedBytes)
+                    {
+                        var mainArray = mainFixed;
+                        if (byteArraySegment.Offset > 0)
+                            mainArray += byteArraySegment.Offset;
+
+                        return OnesSum(mainArray, byteArraySegment.Length, prefixedFixed, prefixedBytes.Length);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 16 bit sum of all values
+        /// http://en.wikipedia.org/wiki/Signed_number_representations#Ones.27_complement
+        /// </summary>
+        /// <param name="mainArray">The main array.</param>
+        /// <param name="mainSize">Size of the main array.</param>
+        /// <param name="prefixArray">The prefix array.</param>
+        /// <param name="prefixSize">Size of the prefix array.</param>
+        /// <returns><see cref="UInt16"/>.</returns>
+        private static unsafe UInt16 OnesSum(byte* mainArray, int mainSize, byte* prefixArray, int prefixSize)
+        {
+            ulong sum = 0;
+
+            sum = Sum(prefixArray, prefixSize, sum);
+            sum = Sum(mainArray, mainSize, sum);
+
+            // Swap the byte order of the sum.
+            sum = SwapBytes(sum);
+            
+            // Folds the sum down to 16 bits.
+            var uint1 = (uint)sum;
+            var uint2 = (uint)(sum >> 32);
+            uint1 += uint2;
+            if (uint1 < uint2)
+                uint1++;
+
+            var ushort1 = (ushort)uint1;
+            var ushort2 = (ushort)(uint1 >> 16);
+            ushort1 += ushort2;
+            if (ushort1 < ushort2)
+                ushort1++;
+
+            return ushort1;
+        }
+
+        /// <summary>
+        /// Calculates the sum of the specified array.
+        /// </summary>
+        /// <param name="array">The array.</param>
+        /// <param name="size">The size.</param>
+        /// <param name="sum">The sum.</param>
+        /// <returns><see cref="System.UInt64"/>.</returns>
+        private static unsafe UInt64 Sum(byte* array, int size, ulong sum)
+        {
+            // Reads per 8 bytes (ulong), this is the main loop.
+            ulong* prefixArrayLong = (ulong*)array;
+            while (size >= 8)
+            {
+                var s = *prefixArrayLong++;
+                sum += s;
+                if (sum < s)
+                    sum++;
+
+                size -= 8;
             }
 
+            // The remainder of the array, which is less than 8 bytes long, needs to be read now.
+            array = (byte*)prefixArrayLong;
 
-            // If the number of prefixed bytes is odd.
-            var byteArraySegmentStartOffset = 0;
-            if (prefixedBytes.Length % 2 == 1 && byteArraySegment.Length > 0)
+            // Reads 4 bytes (uint).
+            if ((size & 4) != 0)
             {
-                sum += prefixedBytes[prefixedBytes.Length - 1] << 8 | byteArraySegment.Bytes[byteArraySegment.Offset];
-                byteArraySegmentStartOffset++;
+                var s = *(uint*)array;
+                sum += s;
+                if (sum < s)
+                    sum++;
+
+                array += 4;
             }
 
-
-            var byteArraySegmentStart = byteArraySegmentStartOffset + byteArraySegment.Offset;
-            var byteArraySegmentEnd = byteArraySegment.Length + byteArraySegment.Offset - 1;
-            for (var i = byteArraySegmentStart; i < byteArraySegmentEnd; i += 2)
+            // Reads 2 bytes (ushort).
+            if ((size & 2) != 0)
             {
-                sum += byteArraySegment.Bytes[i] << 8 |
-                       byteArraySegment.Bytes[i + 1];
+                var s = *(ushort*)array;
+                sum += s;
+                if (sum < s)
+                    sum++;
+
+                array += 2;
             }
 
-
-            // If the number of bytes is odd.
-            if (byteArraySegment.Length % 2 == 1 && byteArraySegmentStartOffset == 0)
+            // Reads 1 byte (byte).
+            if ((size & 1) != 0)
             {
-                if (padOddBytesToLeft)
-                    sum += byteArraySegment.Bytes[byteArraySegment.Offset + byteArraySegment.Length - 1] << 8;
-                else
-                    sum += byteArraySegment.Bytes[byteArraySegment.Offset + byteArraySegment.Length - 1];
-            }
-
-
-            while (sum >> 16 != 0)
-            {
-                sum = (sum & 0xffff) + (sum >> 16);
+                var s = *array;
+                sum += s;
+                if (sum < s)
+                    sum++;
             }
 
             return sum;
+        }
+
+        /// <summary>
+        /// Swaps the order of the bytes in a <c>ulong</c>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns><see cref="System.UInt64"/>.</returns>
+        private static ulong SwapBytes(ulong value)
+        {
+            // Swap adjacent 32-bit blocks.
+            value = (value >> 32) | (value << 32);
+            // Swap adjacent 16-bit blocks.
+            value = ((value & 0xFFFF0000FFFF0000) >> 16) | ((value & 0x0000FFFF0000FFFF) << 16);
+            // Swap adjacent 8-bit blocks.
+            return ((value & 0xFF00FF00FF00FF00) >> 8) | ((value & 0x00FF00FF00FF00FF) << 8);
         }
     }
 }

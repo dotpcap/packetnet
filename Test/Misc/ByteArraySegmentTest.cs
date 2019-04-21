3aa56bceb518603c83b318c8b45716a912857611
PhyxionNL <7643972+PhyxionNL@users.noreply.github.com>
2019-04-21 12:01:04 +0200

    0.30.0
    Bump version.

--- Test/Misc/ByteArraySegmentTest.cs
+++ Test/Misc/ByteArraySegmentTest.cs
@@ -20,7 +20,6 @@ along with PacketDotNet.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
 using NUnit.Framework;
-using PacketDotNet;
 using PacketDotNet.Utils;
 
 namespace Test.Misc

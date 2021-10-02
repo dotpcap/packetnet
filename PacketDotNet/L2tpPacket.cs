a7328f77cf2b59e702ece7ea2786bcc3ff577717
PhyxionNL <7643972+PhyxionNL@users.noreply.github.com>
2021-10-02 17:27:09 +0200

    Add l2tp decode (#151)

--- PacketDotNet/L2tpPacket.cs
+++ PacketDotNet/L2tpPacket.cs
@@ -9,7 +9,6 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *  Copyright 2018 Steven Haufe<haufes@hotmail.com>
   */
 
-using System;
 using System.Text;
 using PacketDotNet.Utils;
 using PacketDotNet.Utils.Converters;
@@ -44,11 +43,12 @@ namespace PacketDotNet
                 Header.Length += L2tpFields.OffsetSizeLength + L2tpFields.OffsetPadLength;
 
             var payload = Header.NextSegment();
+
             try
             {
                 PayloadPacket = new PppPacket(payload) { ParentPacket = this };
             }
-            catch (Exception)
+            catch
             {
                 //it's not a PPP packet, just attach the data
                 PayloadPacketOrData.Value.ByteArraySegment = payload;
@@ -60,19 +60,19 @@ namespace PacketDotNet
         /// <summary>Fetch ascii escape sequence of the color associated with this packet type.</summary>
         public override string Color => AnsiEscapeSequences.DarkGray;
 
-        public bool DataMessage => 8 == (Header.Bytes[Header.Offset] & 0x8);
+        public bool DataMessage => (Header.Bytes[Header.Offset] & 0x8) == 8;
 
-        public bool HasLength => 4 == (Header.Bytes[Header.Offset] & 0x4);
+        public bool HasLength => (Header.Bytes[Header.Offset] & 0x4) == 4;
 
-        public bool HasOffset => 2 == (Header.Bytes[Header.Offset] & 0x2);
+        public bool HasOffset => (Header.Bytes[Header.Offset] & 0x2) == 2;
 
-        public bool HasSequence => 2 == (Header.Bytes[Header.Offset] & 0x2);
+        public bool HasSequence => (Header.Bytes[Header.Offset] & 0x2) == 2;
 
-        public bool IsPriority => 2 == (Header.Bytes[Header.Offset] & 0x2);
+        public bool IsPriority => (Header.Bytes[Header.Offset] & 0x2) == 2;
 
-        public int SessionID => HasLength ? EndianBitConverter.Big.ToUInt16(Header.Bytes, Header.Offset + 5) : EndianBitConverter.Big.ToUInt16(Header.Bytes, Header.Offset + 4);
+        public int SessionId => HasLength ? EndianBitConverter.Big.ToUInt16(Header.Bytes, Header.Offset + 5) : EndianBitConverter.Big.ToUInt16(Header.Bytes, Header.Offset + 4);
 
-        public int TunnelID => HasLength ? EndianBitConverter.Big.ToUInt16(Header.Bytes, Header.Offset + 3) : EndianBitConverter.Big.ToUInt16(Header.Bytes, Header.Offset + 2);
+        public int TunnelId => HasLength ? EndianBitConverter.Big.ToUInt16(Header.Bytes, Header.Offset + 3) : EndianBitConverter.Big.ToUInt16(Header.Bytes, Header.Offset + 2);
 
         public int Version => Header.Bytes[Header.Offset + 1] & 0x7;
 
@@ -90,14 +90,37 @@ namespace PacketDotNet
             if (outputFormat is StringOutputType.Normal or StringOutputType.Colored)
             {
                 // build the output string
-                buffer.AppendFormat("{0}[L2tpPacket",
-                                    color);
+                buffer.AppendFormat("{0}[L2tpPacket", color);
             }
 
             // append the base string output
             buffer.Append(base.ToString(outputFormat));
 
             return buffer.ToString();
+        }
+        
+        /// <summary>
+        /// Determines whether the payload can be decoded by <see cref="L2tpPacket" />.
+        /// </summary>
+        /// <param name="payload">The payload.</param>
+        /// <param name="udpPacket">The UDP packet.</param>
+        /// <returns>
+        /// <c>true</c> if the payload can be decoded by <see cref="L2tpPacket"/>; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool CanDecode(ByteArraySegment payload, UdpPacket udpPacket)
+        {
+            if (udpPacket.SourcePort is L2tpFields.Port || udpPacket.DestinationPort is L2tpFields.Port)
+            {
+                // Ver MUST be 2, indicating the version of the L2TP data message header
+                // described in this document. The value 1 is reserved to permit
+                // detection of L2F [RFC2341] packets should they arrive intermixed with
+                // L2TP packets. Packets received with an unknown Ver field MUST be
+                // discarded.
+
+                return (payload[1] & 0x07) is 1 or 2;
+            }
+
+            return false;
         }
     }
 }

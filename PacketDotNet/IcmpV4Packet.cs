22f3fc31c1f9d6329c577bc204a130f48bbb5f22
PhyxionNL <7643972+PhyxionNL@users.noreply.github.com>
2022-05-13 19:48:21 +0200

    Update IcmpV4Packet.cs

--- PacketDotNet/IcmpV4Packet.cs
+++ PacketDotNet/IcmpV4Packet.cs
@@ -6,10 +6,8 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
 
-using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Threading;
 using PacketDotNet.Utils;
 using PacketDotNet.Utils.Converters;
 

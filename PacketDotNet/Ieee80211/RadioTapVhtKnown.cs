3410e954f5d6194f27583c054de56baaaeca54b8
Jerker <jerker@ecredence.com>
2024-03-07 09:29:02 +0800

    Added comments to RadioTapVhtKnown.cs

--- PacketDotNet/Ieee80211/RadioTapVhtKnown.cs
+++ PacketDotNet/Ieee80211/RadioTapVhtKnown.cs
@@ -2,16 +2,54 @@
 
 namespace PacketDotNet.Ieee80211;
 
+/// <summary>
+/// Indicates the precense of data fields
+/// </summary>
 [Flags]
 public enum RadioTapVhtKnown : ushort
 {
+    /// <summary>
+    /// STBC Known
+    /// </summary>
     StbcKnown                       = 0x0001,
+
+    /// <summary>
+    /// TXOP PS Not Allowed Known
+    /// </summary>
     TxopPsNotAllowedKnown           = 0x0002,
+
+    /// <summary>
+    /// Guard Interval (GI) Known
+    /// </summary>
     GuardIntervalKnown              = 0x0004,
+
+    /// <summary>
+    /// Short GI NSYM Disambiguation Known
+    /// </summary>
     ShortGiNsymDisambiguationKnown  = 0x0008,
+
+    /// <summary>
+    /// LDPC Extra OFDM Sybmol Known
+    /// </summary>
     LdpcExtraOfdmSymbolKnown        = 0x0010,
+
+    /// <summary>
+    /// Beamformed Known
+    /// </summary>
     BeamformedKnown                 = 0x0020,
+
+    /// <summary>
+    /// Bandwidth Known
+    /// </summary>
     BandwidthKnown                  = 0x0040,
+
+    /// <summary>
+    /// Group ID Known
+    /// </summary>
     GroupIdKnown                    = 0x0080,
+
+    /// <summary>
+    /// Partial AID Known
+    /// </summary>
     PartialAidKnown                 = 0x0100
 }

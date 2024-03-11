06f274895d50d2ba7024a66ecfa154a1877f7328
Jerker <jerker@ecredence.com>
2024-03-11 09:13:52 +0800

    Copyright block fix

--- PacketDotNet/Ieee80211/RadioTapHighEfficiencyData3.cs
+++ PacketDotNet/Ieee80211/RadioTapHighEfficiencyData3.cs
@@ -1,8 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+This file is part of PacketDotNet.
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*/
+
+using System;
 
 namespace PacketDotNet.Ieee80211;
 
--- PacketDotNet/Ieee80211/RadioTapVhtBandwidth.cs
+++ PacketDotNet/Ieee80211/RadioTapVhtBandwidth.cs
@@ -1,4 +1,12 @@
-﻿namespace PacketDotNet.Ieee80211;
+﻿/*
+This file is part of PacketDotNet.
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*/
+
+namespace PacketDotNet.Ieee80211;
 
 /// <summary>
 /// Indicates the bandwidth and side channel for a frame capture with VHT

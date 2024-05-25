7d50da195bd75f7b90977faa474ccba2e4fc4534
Jiří Engelthaler <engycz@gmail.com>
2024-05-25 21:10:56 +0200

    Add support for Linux "cooked" capture encapsulation v2 (SLL2) layer type

---
+++ PacketDotNet/LinuxSll2Packet.cs
@@ -0,0 +1,207 @@
+﻿/*
+This file is part of PacketDotNet.
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using PacketDotNet.Utils;
+using PacketDotNet.Utils.Converters;
+
+namespace PacketDotNet;
+
+    /// <summary>
+    /// Represents a Linux cooked capture packet, the kinds of packets
+    /// received when capturing on an 'any' device
+    /// See http://github.com/mcr/libpcap/blob/master/pcap/sll.h
+    /// </summary>
+    public class LinuxSll2Packet : InternetLinkLayerPacket
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="byteArraySegment">
+        /// A <see cref="ByteArraySegment" />
+        /// </param>
+        public LinuxSll2Packet(ByteArraySegment byteArraySegment)
+        {
+            Header = new ByteArraySegment(byteArraySegment) { Length = LinuxSll2Fields.SLL2HeaderLength };
+
+            // parse the payload via an EthernetPacket method
+            PayloadPacketOrData = new LazySlim<PacketOrByteArraySegment>(() => EthernetPacket.ParseNextSegment(Header,
+                                                                                                           EthernetProtocolType));
+        }
+
+        /// <value>
+        /// The encapsulated protocol type
+        /// </value>
+        public EthernetType EthernetProtocolType
+        {
+            get => (EthernetType) EndianBitConverter.Big.ToInt16(Header.Bytes,
+                                                                 Header.Offset + LinuxSll2Fields.EthernetProtocolTypePosition);
+            set
+            {
+                var v = (short) value;
+                EndianBitConverter.Big.CopyBytes(v,
+                                                 Header.Bytes,
+                                                 Header.Offset + LinuxSll2Fields.EthernetProtocolTypePosition);
+            }
+        }
+
+        /// <value>
+        /// Link layer header bytes, maximum of 8 bytes
+        /// </value>
+        public byte[] LinkLayerAddress
+        {
+            get
+            {
+                var headerLength = LinkLayerAddressLength;
+                var theHeader = new byte[headerLength];
+                Array.Copy(Header.Bytes,
+                           Header.Offset + LinuxSll2Fields.LinkLayerAddressPosition,
+                           theHeader,
+                           0,
+                           headerLength);
+
+                return theHeader;
+            }
+            set
+            {
+                // update the link layer length
+                LinkLayerAddressLength = value.Length;
+
+                // copy in the new link layer header bytes
+                Array.Copy(value,
+                           0,
+                           Header.Bytes,
+                           Header.Offset + LinuxSll2Fields.LinkLayerAddressPosition,
+                           value.Length);
+            }
+        }
+
+        /// <value>
+        /// Number of bytes in the link layer address of the sender of the packet
+        /// </value>
+        public int LinkLayerAddressLength
+        {
+            get => Header.Bytes[Header.Offset + LinuxSll2Fields.LinkLayerAddressLengthPosition];
+            set
+            {
+                // range check
+                if (value is < 0 or > 8)
+                {
+                    throw new InvalidOperationException("value of " + value + " out of range of 0 to 8");
+                }
+
+                Header.Bytes[Header.Offset + LinuxSll2Fields.LinkLayerAddressLengthPosition] = (byte) value;
+            }
+        }
+
+        /// <value>
+        /// The
+        /// </value>
+        public int LinkLayerAddressType
+        {
+            get => EndianBitConverter.Big.ToInt16(Header.Bytes,
+                                                  Header.Offset + LinuxSll2Fields.LinkLayerAddressTypePosition);
+            set
+            {
+                var v = (short) value;
+                EndianBitConverter.Big.CopyBytes(v,
+                                                 Header.Bytes,
+                                                 Header.Offset + LinuxSll2Fields.LinkLayerAddressTypePosition);
+            }
+        }
+
+        /// <value>
+        /// Information about the packet direction
+        /// </value>
+        public LinuxSll2Type Type
+        {
+            get => (LinuxSll2Type)Header.Bytes[Header.Offset + LinuxSll2Fields.PacketTypePosition];
+            set
+            {
+                Header.Bytes[Header.Offset + LinuxSll2Fields.PacketTypePosition] = (byte) value;
+            }
+        }
+
+        /// <value>
+        /// Information about the interface index
+        /// </value>
+        public int InterfaceIndex
+        {
+            get => EndianBitConverter.Big.ToInt32(Header.Bytes,
+                                                  Header.Offset + LinuxSll2Fields.InterfaceIndexPosition);
+            set
+            {
+                var v = (int)value;
+                EndianBitConverter.Big.CopyBytes(v,
+                                                 Header.Bytes,
+                                                 Header.Offset + LinuxSll2Fields.InterfaceIndexPosition);
+            }
+        }
+    
+        /// <inheritdoc cref="Packet.ToString(StringOutputType)" />
+        public override string ToString(StringOutputType outputFormat)
+        {
+            var buffer = new StringBuilder();
+            var color = "";
+            var colorEscape = "";
+
+            if (outputFormat is StringOutputType.Colored or StringOutputType.VerboseColored)
+            {
+                color = Color;
+                colorEscape = AnsiEscapeSequences.Reset;
+            }
+
+            if (outputFormat is StringOutputType.Normal or StringOutputType.Colored)
+            {
+                // build the output string
+                buffer.AppendFormat("[{0}LinuxSll2Packet{1}: ProtocolType={2}, InterfaceIndex={3}, LinkLayerAddressType={4}, Type={5}, LinkLayerAddressLength={6}, Source={7}]",
+                                    color,
+                                    colorEscape,
+                                    EthernetProtocolType,
+                                    InterfaceIndex,
+                                    LinkLayerAddressType,
+                                    Type,
+                                    LinkLayerAddressLength,
+                                    BitConverter.ToString(LinkLayerAddress, 0));
+            }
+
+            if (outputFormat is StringOutputType.Verbose or StringOutputType.VerboseColored)
+            {
+                // collect the properties and their value
+                var properties = new Dictionary<string, string>
+                {
+                    { "protocol", EthernetProtocolType + " (0x" + EthernetProtocolType.ToString("x") + ")" },
+                    { "interface index", InterfaceIndex.ToString() },
+                    { "link layer address type", LinkLayerAddressType.ToString() },
+                    { "type", Type + " (" + (int) Type + ")" },
+                    { "link layer address length", LinkLayerAddressLength.ToString() },
+                    { "source", BitConverter.ToString(LinkLayerAddress) },
+                };
+
+                // calculate the padding needed to right-justify the property names
+                var padLength = RandomUtils.LongestStringLength(new List<string>(properties.Keys));
+
+                // build the output string
+                buffer.AppendLine("LCC:  ******* LinuxSll2 - \"Linux Cooked Capture v2\" - offset=? length=" + TotalPacketLength);
+                buffer.AppendLine("LCC:");
+                foreach (var property in properties)
+                {
+                    buffer.AppendLine("LCC: " + property.Key.PadLeft(padLength) + " = " + property.Value);
+                }
+
+                buffer.AppendLine("LCC:");
+            }
+
+            // append the base output
+            buffer.Append(base.ToString(outputFormat));
+
+            return buffer.ToString();
+        }
+    }
---
+++ Test/PacketType/LinuxCookedCapturev2Test.cs
@@ -0,0 +1,135 @@
+﻿/*
+This file is part of PacketDotNet.
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*/
+
+using System;
+using NUnit.Framework;
+using PacketDotNet;
+using SharpPcap;
+using SharpPcap.LibPcap;
+
+namespace Test.PacketType;
+
+    [TestFixture]
+    public class LinuxCookedCapturev2Test
+    {
+        private void VerifyPacket0(Packet p)
+        {
+            // expecting a tcp packet
+            Assert.IsNotNull(p.Extract<TcpPacket>(), "expected a tcp packet");
+        }
+
+        private void VerifyPacket1(Packet p)
+        {
+            // expect a udp packet
+            Assert.IsNotNull(p.Extract<UdpPacket>(), "expected a udp packet");
+        }
+
+        private void VerifyPacket2(Packet p)    
+        {
+            // expect an arp packet
+            var arpPacket = p.Extract<ArpPacket>();
+            Assert.IsNotNull(arpPacket, "Expected arpPacket to not be null");
+
+            // validate some of the LinuxSSLPacket fields
+            var l = (LinuxSll2Packet) p;
+            Assert.AreEqual(2, l.InterfaceIndex, "Interface index");
+            Assert.AreEqual(6, l.LinkLayerAddressLength, "Address length");
+            Assert.AreEqual(1, l.LinkLayerAddressType);
+            Assert.AreEqual(LinuxSll2Type.PacketBroadCast, l.Type);
+
+            // validate some of the arp fields
+            Assert.AreEqual("192.168.178.30",
+                            arpPacket.SenderProtocolAddress.ToString(),
+                            "Arp SenderProtocolAddress");
+
+            Assert.AreEqual("192.168.178.1",
+                            arpPacket.TargetProtocolAddress.ToString(),
+                            "Arp TargetProtocolAddress");
+        }
+
+        [Test]
+        public void CookedCaptureTest()
+        {
+            var dev = new CaptureFileReaderDevice(NUnitSetupClass.CaptureDirectory + "LinuxCookedCapturev2.pcap");
+            dev.Open();
+
+            PacketCapture e;
+            GetPacketStatus status;
+            var packetIndex = 0;
+            while ((status = dev.GetNextPacket(out e)) == GetPacketStatus.PacketRead)
+            {
+                var rawCapture = e.GetPacket();
+                var p = Packet.ParsePacket(rawCapture.GetLinkLayers(), rawCapture.Data);
+                switch (packetIndex)
+                {
+                    case 0:
+                    {
+                        VerifyPacket0(p);
+                        break;
+                    }
+                    case 1:
+                    {
+                        VerifyPacket1(p);
+                        break;
+                    }
+                    case 2:
+                    {
+                        VerifyPacket2(p);
+                        break;
+                    }
+                    default:
+                    {
+                        Assert.Fail("didn't expect to get to packetIndex " + packetIndex);
+                        break;
+                    }
+                }
+
+                packetIndex++;
+            }
+
+            dev.Close();
+        }
+
+        [Test]
+        public void PrintString()
+        {
+            Console.WriteLine("Loading the sample capture file");
+            var dev = new CaptureFileReaderDevice(NUnitSetupClass.CaptureDirectory + "LinuxCookedCapture.pcap");
+            dev.Open();
+            Console.WriteLine("Reading packet data");
+            PacketCapture c;
+            dev.GetNextPacket(out c);
+            var rawCapture = c.GetPacket();
+            var p = Packet.ParsePacket(rawCapture.GetLinkLayers(), rawCapture.Data);
+
+            Console.WriteLine("Parsing");
+            var l = (LinuxSllPacket) p;
+
+            Console.WriteLine("Printing human readable string");
+            Console.WriteLine(l.ToString());
+        }
+
+        [Test]
+        public void PrintVerboseString()
+        {
+            Console.WriteLine("Loading the sample capture file");
+            var dev = new CaptureFileReaderDevice(NUnitSetupClass.CaptureDirectory + "LinuxCookedCapture.pcap");
+            dev.Open();
+            Console.WriteLine("Reading packet data");
+            PacketCapture c;
+            dev.GetNextPacket(out c);
+            var rawCapture = c.GetPacket();
+            var p = Packet.ParsePacket(rawCapture.GetLinkLayers(), rawCapture.Data);
+
+            Console.WriteLine("Parsing");
+            var l = (LinuxSllPacket) p;
+
+            Console.WriteLine("Printing human readable string");
+            Console.WriteLine(l.ToString(StringOutputType.Verbose));
+        }
+    }
